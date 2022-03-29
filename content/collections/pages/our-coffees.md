2c23b8191817e4d3d06abbfcc860e0e7bc6ba425
Joe Grant <jmgrant1995@gmail.com>
2022-03-29 08:22:42 +0800

    setup basic blog template

---
+++ content/collections/pages/our-coffees.md
@@ -0,0 +1,10 @@
+---
+id: fa8a31f8-7958-4d95-9e60-59bde8fb2f0c
+blueprint: pages
+title: 'Our Coffees'
+featured_image: max-fuchs-Nm6ojlDO-5c-unsplash.jpg
+author: 93553f83-2e95-436a-81b8-c71b9a626dff
+template: coffees/index
+updated_by: 93553f83-2e95-436a-81b8-c71b9a626dff
+updated_at: 1648512357
+---
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 module.exports = {
-  content: [
-    './resources/**/*.antlers.html',
-    './resources/**/*.blade.php',
-    './resources/**/*.vue',
-    './content/**/*.md'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+    content: [
+        "./resources/**/*.antlers.html",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.vue",
+        "./content/**/*.md",
+    ],
+    theme: {
+        fontFamily: {
+            sans: ["Roboto", "sans-serif"],
+        },
+        extend: {
+            colors: {
+                dark: "#1B1C1E",
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
+};
